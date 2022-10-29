1ced4ed3eea1d903763e864e9526e2595e875a44
mmansoor05 <69745012+mmansoor05@users.noreply.github.com>
2022-10-28 21:21:09 -0400

    changes to Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Contact from './components/Contact';
@@ -7,17 +8,22 @@ import Resume from './components/Resume';
 import Skills from './components/Skills';
 import Project from './components/Project';
 import About from './components/About';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Nav/>
+      <React.Fragment>
+        <Router  basename={process.env.PUBLIC_URL}>
+        <Nav/>
       <About/>
       <Project/>
       <Skills />
       <Resume />
       <Contact />
       <Footer/>
+      </Router>
+    </React.Fragment>
     </div>
   );
 }
